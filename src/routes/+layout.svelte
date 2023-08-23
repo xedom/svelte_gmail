6efa8d69c6131f60540b5345d22fa9c8c7c6e04e
xedom <xedom08@gmail.com>
2023-08-23 15:31:54 +0200

    main layout, font family

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
+	import './styles.css';
 	import '../app.postcss';
+	import Navbar from '../lib/components/Navbar.svelte';
 </script>
 
-<slot />
+<div class="flex h-screen flex-col">
+	<div class="absolute top-0 -z-10 h-full w-[68px] bg-blue-400"></div>
+	<div class="absolute top-0 -z-20 h-full w-full bg-gray-100"></div>
+	<Navbar />
+	<!-- <div class="ml-[68px] overflow-hidden rounded-lg bg-black p-2"> -->
+	<div class="ml-[324px] flex flex-1 pb-2 pr-2">
+		<div class="flex-1 overflow-hidden rounded-2xl bg-white p-4">
+			<slot />
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
 	console.log('hey');
 </script>
 
-<h1 class="flex flex-col bg-red-500">Welcome to SvelteKit 2</h1>
+<h1>Welcome to SvelteKit 2</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
