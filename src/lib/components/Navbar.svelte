b2decd17bba7c7bd6d2a1a51f703faafb700e388
xedom <xedom08@gmail.com>
2023-08-23 14:30:41 +0200

    add navigation bar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,46 @@
+<script>
+	import DotsGrid from 'svelte-material-icons/DotsGrid.svelte';
+	import Magnify from 'svelte-material-icons/Magnify.svelte';
+	import ChevronDown from 'svelte-material-icons/ChevronDown.svelte';
+	import Menu from 'svelte-material-icons/Menu.svelte';
+	import HelpCircleOutline from 'svelte-material-icons/HelpCircleOutline.svelte';
+	import Cog from 'svelte-material-icons/Cog.svelte';
+	import Close from 'svelte-material-icons/Close.svelte';
+	import Tune from 'svelte-material-icons/Tune.svelte';
+</script>
+
+<header class="z-20 flex items-center p-2 font-semibold text-gray-700">
+	<div class="mr-[148px] flex items-center">
+		<button class="ml-[2px] mr-[10px] rounded-full p-3 hover:bg-gray-300"><Menu class="h-6 w-6" /></button>
+		<div class="px-8 font-bold">Gmail</div>
+	</div>
+
+	<div class="flex flex-1 items-center">
+		<div class="mr-8 flex flex-1 overflow-hidden rounded-full bg-slate-200">
+			<button class="m-1 rounded-full p-2 hover:bg-gray-300"><Magnify class="h-6 w-6" /></button>
+			<input class="h-[46px] flex-1 bg-transparent outline-none" type="text" placeholder="Cerca nella Posta" />
+			<button class="m-1 rounded-full p-2 hover:bg-gray-300"><Close class="h-6 w-6" /></button>
+			<button class="m-1 rounded-full p-2 hover:bg-gray-300"><Tune class="h-6 w-6" /></button>
+		</div>
+		<div class="flex">
+			<button class="mr-3 flex h-[48px] items-center gap-2 rounded-full bg-slate-200 px-2 py-4">
+				<span class="block h-3 w-3 rounded-full bg-green-600" />
+				<span class="text-sm">Online</span>
+				<ChevronDown class="h-4 w-4" />
+			</button>
+			<div class="flex items-center">
+				<button class="m-1 rounded-full p-2 hover:bg-gray-300"><HelpCircleOutline class="h-6 w-6" /></button>
+				<button class="m-1 rounded-full p-2 hover:bg-gray-300"><Cog class="h-6 w-6" /></button>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex items-center justify-self-end">
+		<button class="m-1 rounded-full p-2 hover:bg-gray-300"><DotsGrid class="h-6 w-6" /></button>
+
+		<button class="flex h-[46px] items-center gap-2 rounded-md border-2 border-solid border-gray-300 bg-white p-1">
+			<span>Pingo Corp</span>
+			<img class="square m-1 h-8 w-8 rounded-full object-cover" src="/profile.gif" alt="profile pic" />
+		</button>
+	</div>
+</header>
