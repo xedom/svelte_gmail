7d2d3d86e3cf46fa389906c338c845ff85956372
xedom <xedom08@gmail.com>
2023-08-23 11:32:48 +0200

    initial set up (prettier)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	console.log('hey');
+</script>
+
+<h1 class="flex flex-col bg-red-500">Welcome to SvelteKit 2</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
