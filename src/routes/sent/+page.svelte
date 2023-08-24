95ae7739677a622b704fa4a46e46d7760c704bb3
xedom <xedom08@gmail.com>
2023-08-24 17:19:07 +0200

    added routes for drafts, starred and sent emails

---
+++ src/routes/sent/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { emailStore } from '$lib/store/emails';
+	import InboxEmails from '$lib/components/InboxEmails.svelte';
+	import InboxToolbar from '$lib/components/InboxToolbar.svelte';
+
+	export let data;
+</script>
+
+<InboxToolbar />
+<InboxEmails emails={$emailStore.sent} />
